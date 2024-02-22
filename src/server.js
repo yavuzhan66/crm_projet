0b454e0a8d0ba9af51f571206e2d489bc2bc203e
Yavuzhan <99103025+yavuzhan66@users.noreply.github.com>
2024-02-22 23:52:46 +0100

    o

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const PORT = 8000
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const axios = require('axios')
+
+const app = express()
+
+const url =
+const token = {
+    "clientId": "SZsmfMLgZEzhZrudcfArdkpb",
+    "secret": ".ce5STLMDcfEAu-vrvIuyW6iERtABuZsDk,F_duwovI5SY1M7ik7U5PJZf35nLc2MN8qQ3NYYU_tisGNnsOwg9F0+YakTIW.cBKZP3B1yhEfOSzL-74CmI8G0L4.NNf-",
+    "token": "AstraCS:SZsmfMLgZEzhZrudcfArdkpb:5929b53b618cb8748cd56697b29cc8f2967a26f9eab005e493b62e06b0a45117"
+  } 
